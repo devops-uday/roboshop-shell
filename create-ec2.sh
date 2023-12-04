5cc999e46b9cee61a72db3afa14bd6eec7a23387
udaykiran <yuday0555@gmail.com>
2023-12-04 16:25:08 +0530

    creating ec2 using cli

--- create-ec2.sh
+++ create-ec2.sh
@@ -17,5 +17,5 @@ do
              INSTANCE_TYPE="t2.micro"
     fi
     echo " creating $i instance"
-    aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=intance,Tags=[{Key=Name,Value$i}]"
+    aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=intance,Tags=[{Key=Name,Value=$i}]"
 done    
