84d7baa002af89ac36ced88b586c8fd4090129ea
udaykiran <yuday0555@gmail.com>
2023-12-09 23:02:57 +0530

    create servers

---
+++ create-servers.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#creating and finding Ip Addresses of the created instances using jq(jason queary)
+
+NAMES=$@
+INSTANCE_TYPE=""
+IMAGE_ID=ami-03265a0778a880afb
+SECURITY_GROUP_ID=sg-017e1df2e846425e8
+DOMAIN_NAME=devopsuday.online
+
+#for mysql or mongodb intance_type should be t3.medium and rest of the instances t2.micro
+
+for i in $@
+do 
+   if [[ $i == "mongoDB" || $i == "MySQL" ]]
+         then
+             INSTANCE_TYPE="t3.medium"
+         else
+             INSTANCE_TYPE="t2.micro"
+    fi
+    echo " creating $i instance"
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0].PrivateIpAddress')
+    echo " created instance $i PRIVATE IP ADDRESS is $IP_ADDRESS" 
+
+    aws route53 change-resource-record-sets --hosted-zone-id Z0575256LLP21Z4NFHU7 --change-batch '
+    {
+            "Changes": [{
+            "Action": "CREATE",
+                        "ResourceRecordSet": {
+                                    "Name": "'$i.$DOMAIN_NAME'",
+                                    "Type": "A",
+                                    "TTL": 300,
+                                 "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
+}}]
+}'
+done   
